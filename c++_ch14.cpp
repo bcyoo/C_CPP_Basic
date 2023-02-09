e5539d66352891b3bb307fd9d2cbec8891f8af69
bcyoo <89197951+bcyoo@users.noreply.github.com>
2023-02-09 15:42:04 +0900

    printf, scanf

---
+++ c++_ch14.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h> // 표준 입출력 header
+
+
+int main() {
+
+// printf
+printf("Integer int  %d \n", 10); //정수
+printf("Float %f \n", 3.14f); //실수
+
+for (int i = 0; i < 10; ++i) {
+    printf("Output i : %d\n",i);
+}
+// scanf
+// int iInput = 0;
+// scanf_s("%d", &iInput);
+
+return 0;
+
+}
+
