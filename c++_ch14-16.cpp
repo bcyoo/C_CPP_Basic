3b8cd34ffb6879e192c3d78bbcf6300df284d2a8
bcyoo <89197951+bcyoo@users.noreply.github.com>
2023-02-09 18:31:07 +0900

    printf, scanf, function

--- c++_ch14-16.cpp
+++ c++_ch14-16.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h> // 표준 입출력 header
 
 
+
+int Add(int a, int b) {
+    return a + b;
+}
+
 int main() {
 
 // printf
@@ -13,6 +18,19 @@ for (int i = 0; i < 10; ++i) {
 // scanf
 // int iInput = 0;
 // scanf_s("%d", &iInput);
+
+
+
+// 함수가 사용하는 메모리 영역
+// stack memory area
+
+int iData = Add(100, 200);
+iData = Add(300,  400);
+iData = Add(222,  700);
+iData = Add(333,  555);
+
+
+
 
 return 0;
 
